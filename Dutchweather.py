9972cc2ab62d5291f02e4cfecd35a1dd66db91ba
AndrewSellek <sellek@strw.leidenuniv.nl>
2024-06-14 14:39:42 +0200

    Made face red

--- Dutchweather.py
+++ Dutchweather.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 x = np.linspace(-10,10,21)
 y = 3-0.03 * np.power(x,2)
 
-plt.plot(x,y, linestyle='-', marker='', color='gold')
-plt.plot([-3,3],[5,5], marker='o', linestyle='', color='gold')
+plt.plot(x,y, linestyle='-', marker='', color='red')
+plt.plot([-3,3],[5,5], marker='o', linestyle='', color='red')
 
 plt.xlim([-12,12])
 plt.ylim([-2,10])
-plt.savefig('weather.png')
+plt.savefig('weather_red.png')
