c01340e955282e8617c2c225c5161713665045b9
AndrewSellek <sellek@strw.leidenuniv.nl>
2024-06-14 14:36:46 +0200

    Made weather script

---
+++ Dutchweather.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10,10,21)
+y = 3-0.03 * np.power(x,2)
+
+plt.plot(x,y, linestyle='-', marker='', color='gold')
+plt.plot([-3,3],[5,5], marker='o', linestyle='', color='gold')
+
+plt.xlim([-12,12])
+plt.ylim([-2,10])
+plt.savefig('weather.png')
